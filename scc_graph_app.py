589a69727d3de8e485742f6f782473dbd16e06c3
deepakmahawar150620-beep <deepakmahawar150620@gmail.com>
2025-07-29 15:42:21 +0530

    Update scc_graph_app.py

--- scc_graph_app.py
+++ scc_graph_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/<username>/<repo>/<branch>/<path>/<filename>.xlsx
+url = "https://raw.githubusercontent.com/deepakmahawar150620-beep/VK_Section_SCC/main/Pipeline_VK_Data.xlsx
 "
 df = pd.read_excel(url, engine="openpyxl")
 print("Columns loaded:", df.columns.tolist())
