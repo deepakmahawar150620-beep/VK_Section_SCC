2cbdb8d18a8352874f0d67e31a26c59f35f56129
deepakmahawar150620-beep <deepakmahawar150620@gmail.com>
2025-07-29 15:47:55 +0530

    Update scc_graph_app.py

--- scc_graph_app.py
+++ scc_graph_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/deepakmahawar150620-beep/VK_Section_SCC/main/Pipeline_VK_Data.xlsx
-"
+url = "https://github.com/deepakmahawar150620-beep/VK_Section_SCC/raw/refs/heads/main/Pipeline_VK_Data.xlsx"
 df = pd.read_excel(url, engine="openpyxl")
 print("Columns loaded:", df.columns.tolist())
 print("First row sample:", df.iloc[0].tolist())
